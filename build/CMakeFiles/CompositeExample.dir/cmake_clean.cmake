6b751a4b21f03acc4b2f04418d2b40011dd18e6c
Avokado1024 <slav.kadochnikov@gmail.com>
2024-05-16 03:03:56 +0300

    faile

---
+++ build/CMakeFiles/CompositeExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompositeExample.dir/Composite.cpp.o"
+  "CMakeFiles/CompositeExample.dir/Composite.cpp.o.d"
+  "CompositeExample"
+  "CompositeExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompositeExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
