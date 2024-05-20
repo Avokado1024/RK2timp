4bcaf15eb0d703c265eb9575a568b2e32177cb9c
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2024-05-20 20:16:02 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,63 +1,4 @@
-#include <iostream>
-#include <vector>
-
-class Component {
-public:
-    virtual ~Component() {}
-    virtual Component* getChild(int) { return nullptr; }
-    virtual void add(Component*) {}
-    virtual void remove(int) {}
-    virtual void operation() = 0;
-};
-
-class Composite : public Component {
-public:
-    ~Composite() {
-        for (Component* child : children) {
-            delete child;
-        }
-    }
-
-    Component* getChild(int index) {
-        if (index >= 0 && index < children.size()) {
-            return children[index];
-        }
-        return nullptr;
-    }
-
-    void add(Component* component) {
-        children.push_back(component);
-    }
-
-    void remove(int index) {
-        if (index >= 0 && index < children.size()) {
-            delete children[index];
-            children.erase(children.begin() + index);
-        }
-    }
-
-    void operation() {
-        for (Component* child : children) {
-            child->operation();
-        }
-    }
-
-private:
-    std::vector<Component*> children;
-};
-
-class Leaf : public Component {
-public:
-    Leaf(int i) : id(i) {}
-    ~Leaf() {}
-
-    void operation() {
-        std::cout << "Leaf " << id << " operation" << std::endl;
-    }
-
-private:
-    int id;
-};
+#include "Composite.h" 
 
 void testAddAndGetChild() {
     Composite composite;
@@ -69,7 +10,7 @@ void testAddAndGetChild() {
     } else {
         std::cout << "Test AddAndGetChild: FAILED" << std::endl;
     }
-    delete leaf;
+    delete leaf; // Удаляем объект leaf, чтобы избежать утечки памяти
 }
 
 void testRemoveChild() {
@@ -83,18 +24,20 @@ void testRemoveChild() {
     } else {
         std::cout << "Test RemoveChild: FAILED" << std::endl;
     }
+    delete leaf; // Удаляем объект leaf, чтобы избежать утечки памяти
 }
 
 void testOperation() {
-    Composite composite;
+    Composite* composite = new Composite(); // Создаем объект composite в куче
     Leaf* leaf1 = new Leaf(1);
     Leaf* leaf2 = new Leaf(2);
-    composite.add(leaf1);
-    composite.add(leaf2);
-    composite.operation();
+    composite->add(leaf1);
+    composite->add(leaf2);
+    composite->operation();
     std::cout << "Test Operation: PASSED" << std::endl;
     delete leaf1;
     delete leaf2;
+    delete composite; // Удаляем объект composite, чтобы избежать утечки памяти
 }
 
 int main() {
