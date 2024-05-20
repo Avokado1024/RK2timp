d33f8f8ca59611bef508dc8a03df5bb1e130ed67
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2024-05-20 21:15:25 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,35 +1,50 @@
-#include "Composite.h" 
+#include <iostream>
+#include <cassert>
 
-void testAddAndGetChild() {
+// Подключаем ваш файл с определениями классов
+#include "Composite.h"
+
+// Тесты для класса Composite
+void testComposite() {
     Composite composite;
-    Leaf* leaf = new Leaf(1);
-    composite.add(leaf);
-    Component* retrievedLeaf = composite.getChild(0);
-    if (retrievedLeaf == leaf) {
-        std::cout << "Test AddAndGetChild: PASSED" << std::endl;
-    } else {
-        std::cout << "Test AddAndGetChild: FAILED" << std::endl;
-    }
-    delete leaf; // Удаляем объект leaf, чтобы избежать утечки памяти
+    Leaf leaf1(1);
+    Leaf leaf2(2);
+
+    // Проверка добавления дочерних элементов
+    composite.add(&leaf1);
+    composite.add(&leaf2);
+    assert(composite.getChild(0) == &leaf1);
+    assert(composite.getChild(1) == &leaf2);
+
+    // Проверка удаления дочернего элемента
+    composite.remove(0);
+    assert(composite.getChild(0) == &leaf2);
+    assert(composite.getChild(1) == nullptr);
 }
 
+// Тесты для класса Leaf
+void testLeaf() {
+    Leaf leaf(1);
+    // Проверка операции листа
+    leaf.operation(); // Вывод должен быть виден в консоли
+}
 
+// Тесты для проверки взаимодействия компонентов
+void testCompositeAndLeaf() {
+    Composite composite;
+    Leaf leaf1(1);
+    Leaf leaf2(2);
 
-void testOperation() {
-    Composite* composite = new Composite(); // Создаем объект composite в куче
-    Leaf* leaf1 = new Leaf(1);
-    Leaf* leaf2 = new Leaf(2);
-    composite->add(leaf1);
-    composite->add(leaf2);
-    composite->operation();
-    std::cout << "Test Operation: PASSED" << std::endl;
-    delete leaf1;
-    delete leaf2;
-    delete composite; // Удаляем объект composite, чтобы избежать утечки памяти
+    composite.add(&leaf1);
+    composite.add(&leaf2);
+
+    // Проверка операции композита, которая должна вызвать операцию для каждого листа
+    composite.operation(); // Вывод должен быть виден в консоли для каждого листа
 }
 
 int main() {
-    testAddAndGetChild();
-    testOperation();
-    return 0;
+    testComposite();
+    testLeaf();
+    testCompositeAndLeaf();
+
 }
